60ae52803e5b7848522fe5bb617ed40fc0afa2e1
vishal150520 <vishalkumar150520@gmail.com>
2023-05-12 12:21:47 +0530

    project

---
+++ Migrations/20230506062331_InstallDatabaseSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesCRMApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstallDatabaseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalesLead",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<int>(type: "int", nullable: false),
+                    LastName = table.Column<int>(type: "int", nullable: false),
+                    Mobile = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<int>(type: "int", nullable: false),
+                    Source = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesLead", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesLead");
+        }
+    }
+}
---
+++ Models/SalesLeadEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesCRMApp.Models
+{
+    public class SalesLeadEntity
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Mobile { get; set; }
+        public int Email { get; set; }
+        public int Source { get; set; }
+
+    }
+}
